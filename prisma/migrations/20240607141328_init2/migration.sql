f443942a64fa96452d201a01cb41dfcc131ec391
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-06-07 18:54:06 +0200

    updated migrations

--- prisma/migrations/20240607141328_init2/migration.sql
+++ prisma/migrations/20240607141328_init2/migration.sql
@@ -1,5 +1,18 @@
--- AlterTable
-ALTER TABLE `post` ADD COLUMN `categoryId` INTEGER NULL;
+-- CreateTable
+CREATE TABLE `Post` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(191) NOT NULL,
+    `slug` VARCHAR(191) NOT NULL,
+    `image` VARCHAR(191) NULL,
+    `content` VARCHAR(191) NOT NULL,
+    `published` BOOLEAN NOT NULL,
+    `categoryId` INTEGER NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `Post_slug_key`(`slug`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
 CREATE TABLE `Category` (
