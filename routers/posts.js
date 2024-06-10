fdda00d221e31d1d8b780d81f0539c51f23690ee
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-06-10 09:40:07 +0200

    added validations into posts routers

--- routers/posts.js
+++ routers/posts.js
@@ -4,8 +4,14 @@ const express = require("express");
 // Istanza di express.Router()
 const router = express.Router();
 
-// Importo il viladatore
+// Importo il validatore
 const validator = require('../middlewares/validator.js');
+
+// Importo il validatore dello slug
+const validationSlug = require("../validations/validationSlug.js");
+
+// Importo il bodyData
+const bodyData = require("../validations/posts.js");
 
 // Importo le funzioni dei Posts
 const {
@@ -16,13 +22,16 @@ const {
     destroy
 } = require("../controllers/posts.js");
 
-router.post('/', store);
+router.post('/', validator(bodyData), store);
 
 router.get('/', index);
 
+// Validatore dello slug
+router.use('/:slug', validator(validationSlug));
+
 router.get('/:slug', show);
 
-router.put('/:slug', update);
+router.put('/:slug', validator(bodyData), update);
 
 router.delete('/:slug', destroy);
 
